04d47e5c9e173e3d58767a2e762aa5dc76de55e3
msk <msa4090@naver.com>
2020-08-18 17:30:45 +0900

    2020-08-18 at home

---
+++ test/src/test/ArrayTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ArrayTest {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String[]> arr1 = new ArrayList<>();
+		arr1.add(new String[] {"ㅎ", "ㅇ"});
+		
+		
+		System.out.println(arr1);
+	}
+
+}
